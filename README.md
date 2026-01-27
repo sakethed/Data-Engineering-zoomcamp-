03120d9285a30014f9da5ac8eaa26c7f77066cbb
sakethed <saketh.edpuganti@gmail.com>
2026-01-27 20:55:17 +0000

    restarted the project for better organisation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Data-Engineering-zoomcamp-
 
+This repo shows the work done over the 6 week workshop 
+
+Topics covered are 
+
+1. Docker and terraform 
+2. Workflow orchestration 
+3. Data Warehouse
+4. Analytics engineering 
+5. Batch workloads
+6. Streaming workloads
+
+## Docker and Terraform 
+
+Create and run docker images as containers in a codespace
+
+
+mount and map volumes to a temporary container to run test code 
+
+
+
--- pipeline/test/list_files.py
+++ pipeline/test/list_files.py
@@ -1,4 +1,4 @@
-from pathlib import Path 
+from pathlib import Path
 
 current_dir = Path.cwd()
 current_file = Path(__file__).name
@@ -8,9 +8,8 @@ print(f"Files in {current_dir}:")
 for filepath in current_dir.iterdir():
     if filepath.name == current_file:
         continue
-
     print(f"  - {filepath.name}")
 
     if filepath.is_file():
         content = filepath.read_text(encoding='utf-8')
-        print(f"    Content: {content}")
+        print(f" Content : a{content}")
